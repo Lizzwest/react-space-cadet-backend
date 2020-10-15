c9899437a7ea7d4c61062509f2b65dcb44438918
Martin Briceno <martin.briceno@live.com>
2020-10-15 16:25:55 -0400

    [db]connected

--- backend/models/index.js
+++ backend/models/index.js
@@ -3,11 +3,11 @@ const mongoose = require('mongoose');
 
 // Mongo connection
 // console.log(process.env.MONGO_URI);
-// mongoose.connect(process.env.MONGO_URI, {
-//   useNewUrlParser: true,
-//   useUnifiedTopology: true,
-//   useFindAndModify: true
-// });
+mongoose.connect('mongodb://localhost:27017/cadetdb', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useFindAndModify: true
+});
 
 // Mongoose connection object
 
