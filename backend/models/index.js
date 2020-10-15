3c4af2d6053692066860e7ea1e1900468daae3d5
Martin Briceno <martin.briceno@live.com>
2020-10-15 12:04:59 -0400

    removed all uri to let server run

--- backend/models/index.js
+++ backend/models/index.js
@@ -2,12 +2,12 @@ require('dotenv').config();
 const mongoose = require('mongoose');
 
 // Mongo connection
-console.log(process.env.MONGO_URI);
-mongoose.connect(process.env.MONGO_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-  useFindAndModify: true
-});
+// console.log(process.env.MONGO_URI);
+// mongoose.connect(process.env.MONGO_URI, {
+//   useNewUrlParser: true,
+//   useUnifiedTopology: true,
+//   useFindAndModify: true
+// });
 
 // Mongoose connection object
 
